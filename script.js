5ed86b8e80951ef57c3b5d5560adf859929b251d
OmidQuraishi <omidquraishi.github@gmail.com>
2023-12-24 02:30:01 +0430

    declare virables

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log("hello");
+const check = document.querySelector(".check");
+const again = document.querySelector(".again");
+const guess = document.querySelector(".guess");
+const message = document.querySelector(".message");
+const number = document.querySelector(".number");
+let secrateNumber = Math.floor(Math.random() * 20 + 1);
+
+console.log(secrateNumber);
+
+// check the guess number
+check.addEventListener("click", function () {
+  if (!guess.value) {
+    message.textContent = "Guess a number ⛔";
+  } else if (guess.value > secrateNumber) {
+    message.textContent = "Your Guess is high 📈";
+  } else if (guess.value < secrateNumber) {
+    message.textContent = "Your Guess is low 📉";
+  } else if (guess.value == secrateNumber) {
+    message.textContent = "your win 🎉";
+    document.body.style.backgroundColor = "#60b347";
+  }
+});
