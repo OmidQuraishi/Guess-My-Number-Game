75e0c26d9f10be37227e6a0413fa8fded106497f
OmidQuraishi <omidquraishi.github@gmail.com>
2023-12-25 01:12:01 +0430

    add the again button

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const guess = document.querySelector(".guess");
 const message = document.querySelector(".message");
 const number = document.querySelector(".number");
 let secrateNumber = Math.floor(Math.random() * 20 + 1);
+let scoreNumber = 20;
 
 console.log(secrateNumber);
 
@@ -14,10 +15,26 @@ check.addEventListener("click", function () {
     message.textContent = "Guess a number ⛔";
   } else if (guess.value > secrateNumber) {
     message.textContent = "Your Guess is high 📈";
+    scoreNumber--;
+    document.querySelector(".score").textContent = scoreNumber;
   } else if (guess.value < secrateNumber) {
     message.textContent = "Your Guess is low 📉";
+    scoreNumber--;
+    document.querySelector(".score").textContent = scoreNumber;
   } else if (guess.value == secrateNumber) {
     message.textContent = "your win 🎉";
     document.body.style.backgroundColor = "#60b347";
+    number.textContent = secrateNumber;
+    document.querySelector(".highscore").textContent = scoreNumber;
   }
+});
+
+again.addEventListener("click", function () {
+  secrateNumber = Math.floor(Math.random() * 20 + 1);
+  document.body.style.backgroundColor = "#222";
+  document.querySelector(".score").textContent = 20;
+  scoreNumber = 20;
+  console.log(secrateNumber);
+  guess.value = "";
+  number.textContent = "?";
 });
